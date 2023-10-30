b79ffe3c3a2e9c05a7256424378513f5d5237bbe
Nicolas Pistillo <npistillo@persiscalconsulting.com>
2023-10-30 05:21:04 -0300

    Agrego tailwind, fuentes y paquete de tenancy for laravel

---
+++ resources/views/layouts/basic.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.1/dist/cdn.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Simplecom</title>
+</head>
+
+<body>
+    @yield('content')
+</body>
+</html>
