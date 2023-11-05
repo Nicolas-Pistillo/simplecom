aaadb00232694e6cf94d453939ce6a5b119d94c1
Nicolas Pistillo <npistillo@persiscalconsulting.com>
2023-11-05 00:14:14 -0300

    cambio imagen de prueba para tenants

--- resources/views/superadmin/tenants/index.blade.php
+++ resources/views/superadmin/tenants/index.blade.php
@@ -59,7 +59,7 @@
                       <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
                         <div class="flex items-center">
                           <div class="h-11 w-11 flex-shrink-0">
-                            <img class="h-11 w-11 rounded-full" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                            <img class="h-11 w-11 rounded-full" src="https://images.unsplash.com/photo-1604719312566-8912e9227c6a?auto=format&fit=crop&q=80&w=1374&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
                           </div>
                           <div class="ml-4">
                             <div class="font-medium text-gray-900"> {{ $tenant->ecommerce_name }} </div>
