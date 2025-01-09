b8d7bfeb8408ba6ce301fdb6c2bfa7e5028f7461
Nicolas Pistillo <pistillonicolas@gmail.com>
2025-01-09 02:23:19 -0300

    Avanzo con tarifas de rapiboy y epick

--- app/Services/ShippingProviders/Mocis.php
+++ app/Services/ShippingProviders/Mocis.php
@@ -4,14 +4,16 @@ namespace App\Services\ShippingProviders;
 
 use App\Enums\LogisticType;
 use App\Interfaces\ShippingProvider;
+use App\Services\CartService;
 use App\Traits\Configurable;
+use App\Utils\Address;
 use App\Utils\ShippingRate;
 use App\Utils\ShippingRateParameters;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Str;
+use App\Utils\ShippingBranch;
 
 class Mocis implements ShippingProvider
 {
@@ -46,9 +48,17 @@ class Mocis implements ShippingProvider
 
         if (!$this->token) return $rates;
 
+        $cartPackage = CartService::getPackageInfo('kg');
+
+        $weight = data_get($cartPackage, 'dimensions.weight');
+        $height = data_get($cartPackage, 'dimensions.height');
+        $length = data_get($cartPackage, 'dimensions.length');
+        $width = data_get($cartPackage, 'dimensions.width');
+
         $results = Http::withToken($this->token)
                         ->withBody(json_encode([
-                            'postal_code' => $parameters->recipient_address->zipcode
+                            'postal_code' => $parameters->recipient_address->zipcode,
+                            'items'       => "[\"$weight,$height,$length,$width\"]"
                         ]))
                         ->post("$this->base_url/shipping/price")
                         ->collect('result');
@@ -64,26 +74,27 @@ class Mocis implements ShippingProvider
                 $serviceName = 'Envío a domicilio';
             }
 
-            $estimateDays = now()->diffInDays(Carbon::parse(data_get($result, 'max_delivery')));
+            $fromDays = now()->diffInDays(Carbon::parse(data_get($result, 'min_delivery')));
+            $toDays = now()->diffInDays(Carbon::parse(data_get($result, 'max_delivery')));
 
-            $estimate = "$estimateDays días";
+            $estimate = "$fromDays-$toDays días";
 
-            if ($serviceName === 'Same day')
+            if ($serviceName === 'Same day' || ($fromDays === 0 && $toDays === 1))
             {
                 $estimate = 'Entre hoy y mañana';
             }
 
-            if ($serviceName === 'Next day')
+            if ($serviceName === 'Next day' || ($fromDays === 1 && $toDays === 2))
             {
                 $estimate = 'Entre mañana y pasado';
             }
 
             $rate = new ShippingRate([
                 'source'              => 'mocis',
-                'source_name'         => "Moci's",
+                'source_name'         => "Mocis",
                 'source_data'         => $result,
                 'source_observations' => data_get($result, 'description'),
-                'label'               => "Moci's - $serviceName",
+                'label'               => "Mocis - $serviceName",
                 'carrier_logo'        => URL::to('img/providers/mocis_icon.png'),
                 'service_id'          => data_get($result, 'service.id'),
                 'service_name'        => $serviceName,
@@ -91,6 +102,34 @@ class Mocis implements ShippingProvider
                 'price'               => data_get($result, 'price_iva'),
                 'estimate'            => $estimate
             ]);
+
+            if (data_get($result, 'service.type') === 'pickup' && 
+            !empty(data_get($result, 'pickup')) && !empty(data_get($result, 'address')))
+            {
+                $rate->logistic_type = LogisticType::OriginToDropoff;
+
+                $branchAddress = new Address([
+                    'street'   => data_get($result, 'address.address'),
+                    'number'   => data_get($result, 'address.number'),
+                    'zipcode'  => data_get($result, 'address.zipcode'),
+                    'locality' => data_get($result, 'address.locality') ?? data_get($result, 'address.city'),
+                    'state'    => data_get($result, 'address.province'),
+                    'coordinates' => [
+                        'lat' => data_get($result, 'address.latitude'),
+                        'lng' => data_get($result, 'address.longitude')
+                    ] 
+                ]);
+
+                $rate->branches->push(new ShippingBranch([
+                    'source'      => 'mocis',
+                    'source_name' => "Mocis",
+                    'price'       => data_get($result, 'price_iva'),
+                    'external_id' => data_get($result, 'pickup.id'),
+                    'name'        => data_get($result, 'pickup.name'),
+                    'phone'       => data_get($result, 'address.phone'),
+                    'address'     => $branchAddress
+                ]));
+            }
 
             $rates->push($rate);
         }
