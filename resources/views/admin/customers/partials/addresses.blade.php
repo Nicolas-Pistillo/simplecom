f5cf250af7d7221d644ee85334d50f8ca2ce4f3c
Nicolas Pistillo <pistillonicolas@gmail.com>
2025-04-24 21:57:24 -0300

    Avances en pantalla de detalle de cliente

---
+++ resources/views/admin/customers/partials/addresses.blade.php
@@ -0,0 +1,84 @@
+<div class="py-8">
+    <h2 class="text-base font-semibold text-gray-900 flex items-center gap-1">
+        <x-icon code="location_on" />
+        Direcciones
+    </h2>
+    <div class="mt-4 flow-root">
+        <div class="-mx-4 -my-2 overflow-x-auto" scrollbar-thin>
+            <div class="inline-block min-w-full py-2 align-middle px-3">
+                <div class="overflow-hidden shadow ring-1 ring-black/5 rounded-lg">
+                    @if ($customer->addresses->isEmpty())
+                        <div class="p-4 text-center">
+                            <p class="text-sm text-gray-500">
+                                No hay direcciones registradas por este cliente.
+                            </p>
+                        </div>
+                    @else
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead class="bg-gray-50">
+                                <tr class="text-center">
+                                    <th scope="col"
+                                        class="py-3.5 px-3 text-sm 
+                                    font-semibold text-gray-900">
+                                        Etiqueta
+                                    </th>
+                                    <th scope="col"
+                                        class="py-3.5 px-3 text-sm 
+                                    font-semibold text-gray-900">
+                                        Calle
+                                    </th>
+                                    <th scope="col"
+                                        class="py-3.5 px-3 text-sm 
+                                    font-semibold text-gray-900">
+                                        Detalles
+                                    </th>
+                                    <th scope="col"
+                                        class="px-3 py-3.5 text-sm 
+                                    font-semibold text-gray-900 whitespace-nowrap">
+                                        Código Postal
+                                    </th>
+                                    <th scope="col"
+                                        class="px-3 py-3.5 text-sm 
+                                    font-semibold text-gray-900">
+                                        Localidad
+                                    </th>
+                                    <th scope="col"
+                                        class="px-3 py-3.5 text-sm 
+                                    font-semibold text-gray-900">
+                                        Provincia
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white">
+                                @foreach ($customer->addresses as $address)
+                                    <tr wire:key='address-{{ $address->id }}'
+                                        class="hover:bg-gray-50 cursor-pointer text-center"
+                                        @click="window.open('{{ $address->map_url }}')">
+                                        <td class="whitespace-nowrap py-4 px-3 text-sm font-medium text-gray-900">
+                                            {{ $address->label }}
+                                        </td>
+                                        <td class="whitespace-nowrap py-4 px-3 text-sm font-medium text-gray-900">
+                                            {{ $address->street }} {{ $address->number }}
+                                        </td>
+                                        <td class="whitespace-nowrap py-4 px-3 text-sm font-medium text-gray-900">
+                                            {{ $address->references }}
+                                        </td>
+                                        <td class="whitespace-nowrap py-4 px-3 text-sm font-medium text-gray-900">
+                                            {{ $address->zipcode }}
+                                        </td>
+                                        <td class="whitespace-nowrap py-4 px-3 text-sm font-medium text-gray-900">
+                                            {{ $address->locality }}
+                                        </td>
+                                        <td class="whitespace-nowrap py-4 px-3 text-sm font-medium text-gray-900">
+                                            {{ $address->state }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/admin/customers/show.blade.php
+++ resources/views/livewire/admin/customers/show.blade.php
@@ -1,6 +1,5 @@
 <div>
-    <x-button :href="route('admin.customers.index')" type="secondary" 
-    class="inline-flex items-center gap-1.5 mb-6">
+    <x-button :href="route('admin.customers.index')" type="secondary" class="inline-flex items-center gap-1.5 mb-6">
         <x-icon code="arrow_back" />
         Volver al listado
     </x-button>
@@ -9,8 +8,8 @@
             <div class="shrink-0">
                 <div class="relative">
                     <img class="h-12 w-12 rounded-full"
-                    src="{{ initialsAvatar(['name' => $customer->full_name, 'background' => '#2563eb', 'color' => '#fff']) }}"
-                    alt="Avatar cliente">
+                        src="{{ initialsAvatar(['name' => $customer->full_name, 'background' => '#2563eb', 'color' => '#fff']) }}"
+                        alt="Avatar cliente">
                 </div>
             </div>
             <div class="pt-1.5">
@@ -20,13 +19,67 @@
                 </p>
             </div>
         </div>
-        <div
-            class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
-            <button type="button"
-                class="inline-flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Disqualify</button>
-            <button type="button"
-                class="inline-flex items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Advance
-                to offer</button>
+        <div class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse 
+        sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse 
+        md:mt-0 md:flex-row md:space-x-3">
+            <x-button type="secondary">Disquealify</x-button>
+            <x-button>Advance to offer</x-button>
         </div>
     </div>
+
+    <dl class="mt-6 grid grid-cols-1 text-sm/6 sm:grid-cols-2">
+        <div class="sm:pr-4">
+            <dt class="inline text-gray-500">Fecha de alta</dt>
+            <dd class="inline text-gray-700">
+                <time datetime="2023-23-01">
+                    {{ getElapsedTime($customer->created_at, true) }}
+                </time>
+            </dd>
+        </div>
+        <div class="mt-2 sm:mt-0 sm:pl-4">
+            <dt class="inline text-gray-500">
+                Fecha última act.
+            </dt>
+            <dd class="inline text-gray-700">
+                <time datetime="2023-31-01">
+                    {{ getElapsedTime($customer->updated_at, true) }}
+                </time>
+            </dd>
+        </div>
+        <div class="mt-6 border-t border-gray-900/5 pt-6 sm:pr-4">
+            <dt class="font-semibold text-gray-900">Datos Personales</dt>
+            <dd class="mt-2 text-gray-500 flex flex-col">
+                <span class="font-medium text-gray-900">
+                    Email: {{ $customer->email }}
+                </span>
+                <span class="font-medium text-gray-900">
+                    Teléfono: {{ $customer->phone }}
+                </span>
+                <span class="font-medium text-gray-900">
+                    DNI: {{ $customer->document }}
+                </span>
+            </dd>
+        </div>
+        <div class="mt-8 sm:mt-6 sm:border-t sm:border-gray-900/5 sm:pt-6 sm:pl-4">
+            <dt class="font-semibold text-gray-900">Datos de Facturación</dt>
+            <dd class="mt-2 text-gray-500 flex flex-col">
+                <span class="font-medium text-gray-900">
+                    Cond. fiscal: {{ $customer->tax_condition->name() }}
+                </span>
+                <span class="font-medium text-gray-900">
+                    Razón social: {{ $customer->social_reason ?? '-' }}
+                </span>
+                <span class="font-medium text-gray-900">
+                    CUIT: {{ $customer->invoice_document ?? '-' }}
+                </span>
+                <span class="font-medium text-gray-900">
+                    Domicilio fiscal: {{ $customer->invoice_address ?? '-' }}
+                </span>
+            </dd>
+        </div>
+    </dl>
+
+    @include('admin.customers.partials.addresses')
+
+    @include('admin.customers.partials.orders')
 </div>
