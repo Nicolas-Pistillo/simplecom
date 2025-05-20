861cb24bb2d91c6a7650d675cd9ff1ebcd9b50d5
Nicolas Pistillo <pistillonicolas@gmail.com>
2025-05-20 17:17:56 -0300

    Comienzo modulo de contacto

---
+++ app/Livewire/Ecommerce/Contact.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Ecommerce;
+
+use Livewire\Component;
+
+class Contact extends Component
+{
+    public function render()
+    {
+        return view('livewire.ecommerce.contact');
+    }
+}
